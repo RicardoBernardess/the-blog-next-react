e9f475bbfc0570b64d09d4eb769f35ca0e477a64
Ricardo Bernardes <bernardess.ricardo@gmail.com>
2025-12-08 22:16:30 -0300

    Components anda Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+            priority
+          />
+        </Link>
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time className='text-slate-600 text-sm' dateTime='2025-04-20'>
+            20/04/2025 10:00
+          </time>
+          <PostHeading as='h1' url='#'>
+            Obcaecati quia officiis itaque nemo laudantium tenetur distinctio
+            modi natus?
+          </PostHeading>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius nobis
+            dolorum quam molestias modi ut in deleniti explicabo deserunt
+            quaerat. Obcaecati quia officiis itaque nemo laudantium tenetur
+            distinctio modi natus?
+          </p>
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
