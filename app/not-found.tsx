ac26eceda73e179bd8388365feea34dbcbc816e2
Ricardo Bernardes <bernardess.ricardo@gmail.com>
2025-12-15 17:23:37 -0300

    Slug route

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,21 +2,24 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-80 bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
-        <p>
-          Erro 404 - A página que você está tendando acessar não existe nesse
-          site.
-        </p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-80 bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
+          <p>
+            Erro 404 - A página que você está tendando acessar não existe nesse
+            site.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
